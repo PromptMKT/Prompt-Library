b179d0d2315292cbac350f840e4427950bf6f3dd
vame1404 <150322996+vame1404@users.noreply.github.com>
2026-04-14 21:38:49 +0530

    chore: clean up unnecessary files and fix lint parsing errors in tmp directory

--- parse_prompt_publish.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('Prompt-Library-main-2/src/backend/controllers/PromptController.ts', 'utf8');
-
-const startIndex = code.indexOf('static async publishPrompt(data: any, client?: any) {');
-const endIndex = code.indexOf('static async getPromptById(id: string, client?: any) {');
-
-console.log(code.substring(startIndex, endIndex));
